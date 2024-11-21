27af07cbaa8f4f3637dc24513de3e3a16e559134
ThomasSpoelstra <Thomas_spoel@hotmail.com>
2024-11-21 17:00:59 +0100

    Count words added

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main() :
+    book_path = "books/frankenstein.txt"
+    text = open_book(book_path)
+    number_of_words = count_words(text)
+    print(f"{number_of_words} words found in {book_path}.")
 
-print(file_contents)
+def open_book(book) :
+    with open(book) as f:
+        return f.read()
+
+def count_words(words)  :
+    words = words.split()
+    return len(words)
+
+
+main()
