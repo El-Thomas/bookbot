e36de3dc2b4bdd0273021c57d1719563343ffef3
ThomasSpoelstra <Thomas_spoel@hotmail.com>
2024-11-21 21:04:09 +0100

    Build a sorting system for the characters and looped through them

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 def main() :
+    char_list = []
     book_path = "books/frankenstein.txt"
     text = open_book(book_path)
     number_of_words = count_words(text)
-    number_of_characters = count_characters(text)
+    characters = count_characters(text)
+
+    for char, count in characters.items():
+        char_dict = {
+            "name" : char,
+            "num" : count
+        }
+        char_list.append(char_dict)
     
-    print(f"{number_of_words} words found in {book_path}.")
-    print(f"{number_of_characters} characters found in {book_path}")
+    char_list.sort(reverse=True, key=sort_dictionary)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{number_of_words} words found in {book_path}.\n")
+    
+    for char_dict in char_list:
+        print(f"The '{char_dict['name']}' character was found {char_dict['num']} times")
+
+    print("--- End report ---")
+       
 
 def open_book(book) :
     with open(book) as f:
@@ -16,37 +32,40 @@ def count_words(words)  :
     return len(words)
 
 def count_characters(book):
-    book.lower()  
+    book = book.lower()  
     characters = {
-        'a' : book.find("a"),
-        'b' : book.find("b"),
-        'c' : book.find("c"),
-        'd' : book.find("d"),
-        'e' : book.find("e"),
-        'f' : book.find("f"),
-        'g' : book.find("g"),
-        'h' : book.find("h"),
-        'i' : book.find("i"),
-        'j' : book.find("j"),
-        'k' : book.find("k"),
-        'l' : book.find("l"),
-        'm' : book.find("m"),
-        'n' : book.find("n"),
-        'o' : book.find("o"),
-        'p' : book.find("p"),
-        'q' : book.find("q"),
-        'r' : book.find("r"),
-        's' : book.find("s"),
-        't' : book.find("t"),
-        'u' : book.find("u"),
-        'v' : book.find("v"),
-        'w' : book.find("w"),
-        'x' : book.find("x"),
-        'y' : book.find("y"),
-        'z' : book.find("z")
+        'a' : book.count("a"),
+        'b' : book.count("b"),
+        'c' : book.count("c"),
+        'd' : book.count("d"),
+        'e' : book.count("e"),
+        'f' : book.count("f"),
+        'g' : book.count("g"),
+        'h' : book.count("h"),
+        'i' : book.count("i"),
+        'j' : book.count("j"),
+        'k' : book.count("k"),
+        'l' : book.count("l"),
+        'm' : book.count("m"),
+        'n' : book.count("n"),
+        'o' : book.count("o"),
+        'p' : book.count("p"),
+        'q' : book.count("q"),
+        'r' : book.count("r"),
+        's' : book.count("s"),
+        't' : book.count("t"),
+        'u' : book.count("u"),
+        'v' : book.count("v"),
+        'w' : book.count("w"),
+        'x' : book.count("x"),
+        'y' : book.count("y"),
+        'z' : book.count("z")
     }
     
     return characters
+
+def sort_dictionary(dict) :
+    return dict["num"]
 
 
 main()
